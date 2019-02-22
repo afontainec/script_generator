f2ae8548a6e1656d2c73cca8d9f7b5f54bd33800
Antonio Fontaine <afontaine@uc.cl>
2019-02-22 12:13:57 -0300

    refactor and make directory has place name

--- main.js
+++ main.js
@@ -1,13 +1,43 @@
 
 
 const fs = require('fs-extra');
+let values = require('./data').values;
 
+
+const generateDirectory = (values) => {
+  const dir_name = values['PLACE-NAME'].toUpperCase();
+  return fs.copySync('./MASTER', `./COPIES/${dir_name}`);
+};
 
 const start = async () => {
-  console.log('--');
-  await fs.copySync('./MASTER', './COPIES/MASTER_COPY');
-  console.log('fin');
-}
+  values = getValues();
+  const path = await generateDirectory(values);
+  await compileDirectory(path, values);
+};
+
+const compileDirectory = async () => {
+  return true;
+};
+
+
+const getValues = () => {
+  return {
+    'PLACE-NAME': 'test',
+    'PLACE-ID': 1,
+    SSID: 'free wifi',
+    'VPN-PASSPHRASE': 'asdfasdf',
+    'VPN-USER': 'test',
+    'VPN-PASSWORD': 'test',
+    'PUBLIC-IP': '8.8.8.8',
+    'ISP-GATEWAY': '8.8.8.8',
+    'ISP1-GATEWAY': '8.8.8.8',
+    'ISP2-GATEWAY': '8.8.8.8',
+    'ISP1-ADDRESS': '8.8.8.8',
+    'ISP2-ADDRESS': '8.8.8.8',
+    'MAC-AP1': 'AA:AA:AA:AA:AA:AA',
+    'MAC-AP2': 'AA:AA:AA:AA:AA:AB',
+  };
+};
 
 
 start();
