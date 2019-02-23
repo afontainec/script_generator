70703451941535b117fc8dc8db08b3f99718f140
Antonio Fontaine <afontaine@uc.cl>
2019-02-22 22:20:49 -0300

    prompt for asking information

--- data.js
+++ data.js
@@ -1,7 +1,10 @@
+const requestify = require('requestify');
+const prompt = require('prompt');
+
 const AP_PREFIX = '10.5.49.';
 const AP_NUM = 100;
-const requestify = require('requestify');
 
+prompt.start();
 exports.get = async () => {
   const values = {};
   await getName(values);
@@ -9,21 +12,30 @@ exports.get = async () => {
   await vpn(values);
   await getInternetConexion(values);
   await getAPs(values);
+  console.log('-------------');
+  console.log(values);
   return values;
 };
 
 const getName = async(values) => {
-  const answer = await userInput('Nombre de la red:');
+  const promptParams = {};
+  promptParams.name = 'nombre';
+  promptParams.validator = /^[a-zA-Z\s-]+$/;
+  promptParams.warning = 'Invalid data, it can only contains letters, spaces, or dashes';
+  const answer = await userInput('Nombre de la red:', promptParams);
   values.name = answer;
   const search = new RegExp(' ', 'g');
   values['PLACE-NAME'] = answer.replace(search, '_');
 };
 
 const getID = async(values) => {
-  const answer = await userInput('Red ya esta registrada en www.accionet.net? ingrese y si esta cualquier otra tecla si esque no');
+  const promptParams = {};
+  promptParams.name = 'registrado?';
+  const answer = await userInput('Red ya esta registrada en www.accionet.net? ingrese y si esta cualquier otra tecla si esque no', promptParams);
   let id;
   if (answer === 'y') {
-    id = await userInput('Ingrese ID de la red en www.accionet.net');
+    promptParams.name = 'ID';
+    id = await userInput('Ingrese ID de la red en www.accionet.net', promptParams);
   } else {
     id = await createPlace(values);
   }
@@ -31,27 +43,42 @@ const getID = async(values) => {
 };
 
 const vpn = async (values) => {
-  const answer = await userInput('Configurar VPN para esta red? ingrese y si esta cualquier otra tecla si esque no');
+  const promptParams = {};
+  promptParams.name = 'vpn?';
+  const answer = await userInput('Configurar VPN para esta red? ingrese y si esta cualquier otra tecla si esque no', promptParams);
   if (answer !== 'y') return;
-  values['VPN-PASSPHRASE'] = await userInput('  Ingrese passphrase:');
-  values['VPN-USER'] = await userInput('  Ingrese nombre de usuario:');
-  values['VPN-PASSWORD'] = await userInput('  Ingrese contrasena:');
+  promptParams.name = 'usuario';
+  values['VPN-USER'] = await userInput('  Ingrese nombre de usuario:', promptParams);
+  promptParams.hidden = true;
+  promptParams.replace = '*';
+  promptParams.name = 'passphrase';
+  values['VPN-PASSPHRASE'] = await userInput('  Ingrese passphrase:', promptParams);
+  promptParams.name = 'contrasena';
+  values['VPN-PASSWORD'] = await userInput('  Ingrese contrasena:', promptParams);
 };
 
 const getInternetConexion = async (values) => {
-  const answer = await userInput('La conexión a internet se dará por Enlace dedicado?  ingrese y si esta cualquier otra tecla si esque no');
+  const promptParams = {};
+  promptParams.name = 'conexion';
+  const answer = await userInput('La conexión a internet se dará por Enlace dedicado?  ingrese y si esta cualquier otra tecla si esque no', promptParams);
   if (answer === 'y') return dedicatedInternet(values);
 };
 
 const dedicatedInternet = async (values) => {
-  values['PUBLIC-IP'] = await userInput('  Ingrese IP pública:');
-  values['ISP-GATEWAY'] = await userInput('  Ingrese Puerta de Enlace de la IP pública:');
+  const promptParams = {};
+  promptParams.name = 'IP';
+  values['PUBLIC-IP'] = await userInput('  Ingrese IP pública:', promptParams);
+  promptParams.name = 'gateway';
+  values['ISP-GATEWAY'] = await userInput('  Ingrese Puerta de Enlace de la IP pública:', promptParams);
 };
 
 const getAPs = async (values) => {
-  const answer = await userInput('Desea usar el AP del mismo router?');
+  const promptParams = {};
+  promptParams.name = 'router_ap?';
+  const answer = await userInput('Desea usar el AP del mismo router?', promptParams);
   if (answer === 'y') routerAP(values);
-  const n = await userInput('Cuantos dispositivos de red desea conectar al router? (AP, switch, controladores, etc)');
+  promptParams.name = 'cantidad';
+  const n = await userInput('Cuantos dispositivos de red desea conectar al router? (AP, switch, controladores, etc)', promptParams);
   values.numOfAps = parseInt(n, 10);
   for (let i = 0; i < values.numOfAps; i++) {
     await getAP(values, i);
@@ -59,7 +86,9 @@ const getAPs = async (values) => {
 };
 
 const routerAP = async (values) => {
-  values.SSID = await userInput('  Ingrese Nombre de la señal de wifi(SSID):');
+  const promptParams = {};
+  promptParams.name = 'SSID';
+  values.SSID = await userInput('  Ingrese Nombre de la señal de wifi(SSID):', promptParams);
   values.useRouterAP = true;
 };
 
@@ -67,7 +96,9 @@ const getAP = async (values, i) => {
   i++;
   const macKey = `MAC-AP${i}`;
   const ipKey = `IP-AP${i}`;
-  values[macKey] = await userInput(`  Ingrese dirección mac del dispotivo num ${i}:`);
+  const promptParams = {};
+  promptParams.name = 'mac-address';
+  values[macKey] = await userInput(`  Ingrese dirección mac del dispotivo num ${i}:`, promptParams);
   values[ipKey] = getAP_IP(i);
 };
 
@@ -81,17 +112,27 @@ const createPlace = async(values) => {
   data.place = {};
   data.place.name = values.name;
   data.place.location = values.location;
-  // const response = await requestify.post('http://localhost:3000/places/new', data);
+  // const response = await requestify.post('http://localhost:3000/api/v1/places/new', data);
   // return response.id;
   return 1;
 };
 
-const responses = ['testing hola', 'n', 'y', 'passphrase', 'vpnuser', 'vpnpassword', 'n', 'y', 'FREE WIFI', 2, 'AA:AA:AA:AA:AA:AA', 'AA:AA:AA:AA:AA:AB'];
-let i = 0;
-const userInput = async (question) => {
-  console.log(question);
-  const ans = responses[i];
-  i++;
-  console.log('    user:', ans);
-  return ans;
+// const responses = ['testing hola', 'n', 'y', 'passphrase', 'vpnuser', 'vpnpassword', 'n', 'y', 'FREE WIFI', 2, 'AA:AA:AA:AA:AA:AA', 'AA:AA:AA:AA:AA:AB'];
+// let i = 0;
+// const userInput = async (question, name) => {
+//   console.log(question);
+//   const ans = responses[i];
+//   i++;
+//   console.log('    user:', ans);
+//   return ans;
+// };
+
+const userInput = (question, params) => {
+  return new Promise((resolve, reject) => {
+    console.log(question);
+    prompt.get(params, (err, result) => {
+      if (err) return reject(err);
+      resolve(result[params.name]);
+    });
+  });
 };
---
+++ prompt.js
@@ -0,0 +1,54 @@
+// Include prompt module.
+const prompt = require('prompt');
+
+// This json object is used to configure what data will be retrieved from command line.
+const prompt_attributes = [
+  {
+        // The fist input text is assigned to username variable.
+    name: 'username',
+        // The username must match below regular expression.
+    validator: /^[a-zA-Z\s-]+$/,
+        // If username is not valid then prompt below message.
+    warning: 'Username is not valid, it can only contains letters, spaces, or dashes',
+  },
+  {
+        // The second input text is assigned to password variable.
+    name: 'password',
+        // Do not show password when user input.
+    hidden: true,
+    replace: '*',
+  },
+  {
+        // The third input text is assigned to email variable.
+    name: 'email',
+        // Display email address when user input.
+    hidden: false,
+  }, {
+          // The third input text is assigned to email variable.
+    name: 'number',
+          // Display email address when user input.
+    type: 'number',
+  },
+];
+
+// Start the prompt to read user input.
+prompt.start();
+
+// Prompt and get user input then display those data in console.
+prompt.get(prompt_attributes, (err, result) => {
+  if (err) {
+    console.log(err);
+    return 1;
+  }
+  console.log('Command-line received data:');
+
+        // Get user input from result object.
+  const username = result.username;
+  const password = result.password;
+  const email = result.email;
+  const message = `  Username : ${username} , Password : ${password} , Email : ${email}`;
+
+        // Display user input in console log.
+  console.log(message);
+  console.log(result.number);
+});
